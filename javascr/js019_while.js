2d8d78cf89c05834787fd48f490e87b689933882
Jang <nnazgul1120@gmail.com>
2022-09-20 18:22:41 +0900

    0920

---
+++ javascr/js019_while.js
@@ -0,0 +1,28 @@
+/*
+while(조건식){
+    실행문;
+}
+
+
+*/
+
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+let j = 1;
+while (j <= 5) {
+  console.log(j);
+  j++;
+}
+
+/*
+do{
+    실행문;
+}while(조건식);
+*/
+
+let input = "n";
+do {
+  console.log(`계속하시겠습니까?(y/n)`);
+} while (input == "y");
